5adba20dafb787a128f1d6dc6325ea134cc31c1e
Zeidman Ron <ronronz145@gmail.com>
2023-08-14 20:19:39 +0300

    style: enhance visuals of ExerciseCard

--- exbuilder-front/src/components/ExerciseCard.tsx
+++ exbuilder-front/src/components/ExerciseCard.tsx
@@ -1,19 +1,21 @@
-import React from "react";
-import { Exercise } from "../models/Exercise";
+import React from 'react';
+import { Exercise } from '../models/Exercise';
+import './ExerciseCard.css'; // Import the CSS file for styling
 
 interface ExerciseCardProps {
   exercise: Exercise;
 }
+
 const ExerciseCard: React.FC<ExerciseCardProps> = ({ exercise }) => {
   return (
     <div className="exercise-card">
-      <h2>{exercise.Title}</h2>
-      <p>
-        {exercise.Type}, {exercise.BodyPart}, {exercise.Equipment},{" "}
-        {exercise.Level}
-      </p>
-
-
+      <h2 className="exercise-title">{exercise.Title}</h2>
+      <div className="exercise-details">
+        <p><strong>Type:</strong> {exercise.Type}</p>
+        <p><strong>Body Part:</strong> {exercise.BodyPart}</p>
+        <p><strong>Equipment:</strong> {exercise.Equipment}</p>
+        <p><strong>Level:</strong> {exercise.Level}</p>
+      </div>
     </div>
   );
 };
--- exbuilder-front/src/components/ExerciseList.tsx
+++ exbuilder-front/src/components/ExerciseList.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react";
-import { Exercise } from "../models/Exercise";
-import { fetchExercises } from "../api/ExerciseApi";
-import ExerciseCard from "./ExerciseCard";
+import React, { useEffect, useState } from 'react';
+import { Exercise } from '../models/Exercise';
+import { fetchExercises } from '../api/ExerciseApi';
+import ExerciseCard from './ExerciseCard';
 
 function ExerciseList() {
   const [exercises, setExercises] = useState<Exercise[]>([]);
@@ -9,17 +9,17 @@ function ExerciseList() {
   useEffect(() => {
     fetchExercises()
       .then((data) => setExercises(data))
-      .catch((error) => console.error("Error fetching exercises:", error));
+      .catch((error) => console.error('Error fetching exercises:', error));
   }, []);
 
   return (
-    <div>
+    <div className="exercise-list">
       <h1>Exercise List</h1>
-      {exercises.map((exercise, index) => (
-        <div key={index} className="exercise">
-         <ExerciseCard exercise={exercise}/>
-        </div>
-      ))}
+      <div className="exercise-cards">
+        {exercises.map((exercise, index) => (
+          <ExerciseCard key={index} exercise={exercise} />
+        ))}
+      </div>
     </div>
   );
 }
