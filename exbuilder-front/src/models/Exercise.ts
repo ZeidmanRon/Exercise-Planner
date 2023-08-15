8b4169101711330a13847f38ec321323b3313fc4
Zeidman Ron <ronronz145@gmail.com>
2023-08-16 02:45:54 +0300

    feat: Implement Exercise CRUD Functionality
    - Add option to display delete button in ExerciseCard component
    - Update ExerciseForm component to generate UUID for new exercises
    - Modify backend to handle UUIDs for new exercises
    - Update frontend API to handle UUIDs for exercises
    - Update server API to handle UUIDs for exercises
    - Update ExerciseForm component to reset ID field after submission
    - Update ExerciseData JSON with valid UUIDs for exercises
    
    This commit implements CRUD functionality for exercises, allowing users to add and delete exercises with proper UUIDs. The frontend and backend have been synchronized to handle UUIDs consistently.

--- exbuilder-front/src/models/Exercise.ts
+++ exbuilder-front/src/models/Exercise.ts
@@ -1,12 +1,11 @@
 // exercises-frontend/src/models/Exercise.ts
 
 export interface Exercise {
+  id: string;
   Title: string;
   Desc: string;
   Type: string;
   BodyPart: string;
   Equipment: string;
   Level: string;
-  Rating: string;
-  RatingDesc: string;
 }
--- exbuilder-server/exerciseData.js
+++ exbuilder-server/exerciseData.js
@@ -1,13 +1,35 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const dataFilePath = path.join(__dirname, 'exerciseData.json');
+const dbFilePath = path.join(__dirname, "exerciseData.json");
 
-function readExerciseData() {
-  const data = fs.readFileSync(dataFilePath, 'utf8');
+function getExercises() {
+  const data = fs.readFileSync(dbFilePath, "utf8");
   return JSON.parse(data);
 }
 
+const addExercise = (exercise) => {
+  const exercises = getExercises();
+  exercises.push(exercise);
+  saveExercises(exercises);
+
+  return exercise; // Return the added exercise
+};
+
+const saveExercises = (exercises) => {
+  fs.writeFileSync(dbFilePath, JSON.stringify(exercises, null, 2));
+};
+
+const deleteExercise = (exerciseId) => {
+  const exercises = getExercises();
+  const updatedExercises = exercises.filter(
+    (exercise) => exercise.id !== exerciseId
+  );
+  saveExercises(updatedExercises);
+};
+
 module.exports = {
-  readExerciseData,
+  getExercises,
+  addExercise,
+  deleteExercise,
 };
